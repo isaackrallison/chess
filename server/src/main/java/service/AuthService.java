00941235de2f0fde75f50b76819d87d49b009fcc
Isaac Rallison <isaacrallison@gmail.com>
2024-10-22 12:30:51 -0600

    Creating Auth Service

---
+++ server/src/main/java/service/AuthService.java
@@ -0,0 +1,33 @@
+package service;
+
+import dataaccess.AuthDAO;
+import dataaccess.UserDAO;
+import model.AuthData;
+
+public class AuthService {
+    private final AuthDAO authDAO;
+    private final UserDAO userDAO;
+
+    public AuthService(AuthDAO authDAO, UserDAO userDAO) {
+        this.authDAO = authDAO;
+        this.userDAO = userDAO;
+    }
+
+    public AuthData verifyAuth(String authToken) {
+        AuthData authData = authDAO.getAuthToken(authToken);
+        if (authData == null) {
+            throw new RuntimeException("Invalid auth token");
+        }
+        return authData; // Return the AuthData if the token is valid
+    }
+
+    public void logout(String authToken) {
+        AuthData authData = authDAO.getAuthToken(authToken);
+        if (authData == null) {
+            throw new RuntimeException("Invalid auth token");
+        }
+        authDAO.deleteAuthToken(authToken); // Remove the auth token
+    }
+}
+
+
